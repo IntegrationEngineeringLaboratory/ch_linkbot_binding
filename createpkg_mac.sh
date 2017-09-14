c05a2c76249e22641782299808ad79fda1752c66
David Ko <david.dk.ko@gmail.com>
2017-09-14 10:23:34 -0700

    Updated package creation script

--- createpkg_mac.sh
+++ createpkg_mac.sh
@@ -8,15 +8,11 @@ echo Building $PACKAGE-$VERSION-$ARCH.zip ...
 rm -rf $PACKAGE-$VERSION-$ARCH
 rm -rf $PACKAGE-$VERSION-$ARCH.zip
 mkdir -p $PKGDIR
-mkdir $PKGDIR/lib
 mkdir $PKGDIR/demos
-mkdir $PKGDIR/dl
-mkdir $PKGDIR/include
 mkdir $PKGDIR/docs
 mkdir $PKGDIR/bin
-cp chlinkboti.chf $PKGDIR/lib
-cp chlinkbotl.chf $PKGDIR/lib
-cp liblinkbot.dl $PKGDIR/dl
-cp linkbot.h $PKGDIR/include
+cp -R chbarobo/lib $PKGDIR/lib
+cp -R chbarobo/dl $PKGDIR/dl
+cp -R chbarobo/include $PKGDIR/include 
 #cp dlls/$ARCH/* $PKGDIR/bin
 zip -rq $PACKAGE-$VERSION-$ARCH.zip $PACKAGE-$VERSION-$ARCH
